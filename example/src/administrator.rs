b1b00b48c544c8386a9bf1484a4f7a724ebf178b
Andrew Prindle <prindle.andrew@gmail.com>
2017-11-12 04:41:09 -0600

    Added some more documentation comments

--- example/src/administrator.rs
+++ example/src/administrator.rs
@@ -48,13 +48,26 @@ impl AuthorizeCookie for AdministratorCookie {
     /// serde_json or serde's messagepack implementation ( rmp-serde [rmps]).
     /// 
     /// Json is portable and human readable.  
-    ///
-    /// MsgPack
+    /// 
+    /// MsgPack is a binary format, and while not human readable is more
+    /// compact and efficient.
     fn store_cookie(&self) -> String {
         // String::from("This is my cooky")
         // let ser = ::serde_json::to_string(self).expect("Could not serialize");
         ::serde_json::to_string(self).expect("Could not serialize")
     }
+    
+    
+    /// The retrieve_cookie() method deserializes a string
+    /// into a cookie data type.
+    /// 
+    /// Again, serde is likely to be used here.
+    /// Either the messagepack or json formats would work well here.
+    /// 
+    /// Json is portable and human readable.  
+    /// 
+    /// MsgPack is a binary format, and while not human readable is more
+    /// compact and efficient.
     #[allow(unused_variables)]
     fn retrieve_cookie(string: String) -> Option<Self::CookieType> {
         let mut des_buf = string.clone();
@@ -77,6 +90,7 @@ impl AuthorizeCookie for AdministratorCookie {
 impl AuthorizeForm for AdministratorForm {
     type CookieType = AdministratorCookie;
     
+    /// Authenticate the credentials inside the login form
     fn authenticate(&self) -> Result<Self::CookieType, AuthFail> {
         println!("Authenticating {} with password: {}", &self.username, &self.password);
         if &self.username == "administrator" && &self.password != "" {
@@ -94,6 +108,7 @@ impl AuthorizeForm for AdministratorForm {
         }
     }
     
+    /// Create a new login form instance
     fn new_form(user: &str, pass: &str) -> Self {
         AdministratorForm {
             username: user.to_string(),
@@ -105,7 +120,17 @@ impl AuthorizeForm for AdministratorForm {
 
 impl<'a, 'r> FromRequest<'a, 'r> for AdministratorCookie {
     type Error = ();
-
+    
+    /// The from_request inside the file defining the custom data types
+    /// enables the type to be checked directly in a route as a request guard
+    /// 
+    /// This is not needed but highly recommended.  Otherwise you would need to use:
+    /// 
+    /// `#[get("/protected")] fn admin_page(admin: AuthCont<AdministratorCookie>)`
+    /// 
+    /// instead of:
+    /// 
+    /// `#[get("/protected")] fn admin_page(admin: AdministratorCookie)`
     fn from_request(request: &'a Request<'r>) -> ::rocket::request::Outcome<AdministratorCookie,Self::Error>{
         let cid = AdministratorCookie::cookie_id();
         let mut cookies = request.cookies();
--- src/authorization.rs
+++ src/authorization.rs
@@ -51,9 +51,16 @@ pub trait CookieId {
 }
 
 pub trait AuthorizeCookie : CookieId {
+    /// CookieType is the data type that will hold the cookie information
     type CookieType: AuthorizeCookie;
+    
+    /// Serialize the cookie data type - must be implemented by cookie data type
     fn store_cookie(&self) -> String;
+    
+    /// Deserialize the cookie data type - must be implemented by cookie data type
     fn retrieve_cookie(String) -> Option<Self> where Self: Sized;
+    
+    /// Deletes a cookie.  This does not need to be implemented.
     fn delete_cookie(mut cookies: Cookies) {
         cookies.remove_private( 
            Cookie::named( Self::cookie_id() )
@@ -65,12 +72,24 @@ pub trait AuthorizeForm : CookieId {
     // Todo: remove this associated type, no longer used
     type CookieType: AuthorizeCookie;
     
+    /// Determine whether the login form structure containts
+    /// valid credentials, otherwise send back the username and
+    /// a message indicating why it failed in the `AuthFail` struct
+    /// 
+    /// Must be implemented on the login form structure
     fn authenticate(&self) -> Result<Self::CookieType, AuthFail>;
     
+    /// Create a new login form Structure with 
+    /// the specified username and password
+    /// 
+    /// Must be implemented on the login form structure
     fn new_form(&str, &str) -> Self;
     
-    /// The fail_url() method is used to create a url that the user is sent
-    /// to when the authentication fails.  The
+    /// The `fail_url()` method is used to create a url that the user is sent
+    /// to when the authentication fails.  The default implementation
+    /// redirects the user to the /page?user=<ateempted_username>
+    /// which enables the form to display the username that was attempted
+    /// and unlike FlashMessages it will persist across refreshes
     fn fail_url(user: &str) -> String {
         let mut output = String::with_capacity(user.len() + 10);
         output.push_str("?user=");
@@ -78,6 +97,13 @@ pub trait AuthorizeForm : CookieId {
         output
     }
     
+    /// Redirect the user to one page on successful authentication or
+    /// another page (with a `FlashMessage` indicating why) if authentication fails.
+    /// 
+    /// `FlashMessage` is used to indicate why the authentication failed
+    /// this is so that the user can see why it failed but when they refresh
+    /// it will disappear, enabling a clean start, but with the user name
+    /// from the url's query string (determined by `fail_url()`)
     fn flash_redirect(&self, ok_redir: &str, err_redir: &str, mut cookies: Cookies) -> Result<Redirect, Flash<Redirect>> {
         match self.authenticate() {
             Ok(cooky) => {
@@ -93,6 +119,27 @@ pub trait AuthorizeForm : CookieId {
                     furl.push_str(&furl_qrystr);
                 }
                 Err( Flash::error(Redirect::to(&furl), &fail.msg) )
+            },
+        }
+    }
+    
+    /// Redirect the user to one page on successful authentication or
+    /// another page if authentication fails.
+    fn flash_redirect(&self, ok_redir: &str, err_redir: &str, mut cookies: Cookies) -> Result<Redirect, Redirect> {
+        match self.authenticate() {
+            Ok(cooky) => {
+                let cid = Self::cookie_id();
+                let contents = cooky.store_cookie();
+                cookies.add_private(Cookie::new(cid, contents));
+                Ok(Redirect::to(ok_redir))
+            },
+            Err(fail) => {
+                let mut furl = String::from(err_redir);
+                if &fail.user != "" {
+                    let furl_qrystr = Self::fail_url(&fail.user);
+                    furl.push_str(&furl_qrystr);
+                }
+                Err( Redirect::to(&furl), &fail.msg )
             },
         }
     }
