3baf7d4bed21f73a3f1eea90f5fac95c20025c82
Andrew Prindle <prindle.andrew@gmail.com>
2017-11-12 02:12:24 -0600

    Fixed some bugs in the example.

---
+++ example/target/debug/deps/rocket_auth_login_example-c47ee7ce4bbb56fa.d
@@ -0,0 +1,7 @@
+C:\code\lang\Rust\proj\rocket-auth-login\example\target\debug\deps\rocket_auth_login_example-c47ee7ce4bbb56fa.exe: src\main.rs src\administrator.rs src\layout.rs
+
+C:\code\lang\Rust\proj\rocket-auth-login\example\target\debug\deps\rocket_auth_login_example-c47ee7ce4bbb56fa.d: src\main.rs src\administrator.rs src\layout.rs
+
+src\main.rs:
+src\administrator.rs:
+src\layout.rs:
