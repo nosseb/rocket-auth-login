45cb5707af02d45887248a1bc065a3587475c6bf
Andrew Prindle <prindle.andrew@gmail.com>
2017-11-12 16:56:53 -0600

    Initial commit.  Non compiling commit.  Adding database functionality to the example.

---
+++ example/database_example/src/administrator.rs
@@ -0,0 +1,154 @@
+
+use rocket::{Request, Outcome};
+use rocket::request::FromRequest;
+use std::collections::HashMap;
+// use rocket::{Request, Data, Outcome, Response};
+// use rocket::http::{Cookie, Cookies, MediaType, ContentType, Status, RawStr};
+// use rocket::request::{FlashMessage, Form, FromRequest,FromForm, FormItems, FromFormValue, FromParam};
+// use rocket::response::{content, NamedFile, Redirect, Flash, Responder, Content};
+// use rocket::response::content::Html;
+
+use auth::authorization::*;
+// use auth::sanitization::*;
+
+/// The AdministratorCookie type is used to indicate a user has logged in as an administrator
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AdministratorCookie {
+    pub userid: u32,
+    pub username: String,
+    pub display: Option<String>,
+}
+
+/// The AdministratorForm type is used to process a user attempting to login as an administrator
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AdministratorForm {
+    pub username: String,
+    pub password: String,
+}
+
+impl CookieId for AdministratorCookie {
+    fn cookie_id<'a>() -> &'a str {
+        "acid"
+    }
+}
+
+impl CookieId for AdministratorForm {
+    fn cookie_id<'a>() -> &'a str {
+        "acid"
+    }
+} 
+
+impl AuthorizeCookie for AdministratorCookie {
+    // type CookieType = AdministratorCookie;
+    
+    /// The store_cookie() method should contain code that
+    /// converts the specified data structure into a string
+    /// 
+    /// This is likely to be achieved using one of the serde
+    /// serialization crates.  Personally I would use either
+    /// serde_json or serde's messagepack implementation ( rmp-serde [rmps]).
+    /// 
+    /// Json is portable and human readable.  
+    /// 
+    /// MsgPack is a binary format, and while not human readable is more
+    /// compact and efficient.
+    fn store_cookie(&self) -> String {
+        // String::from("This is my cooky")
+        // let ser = ::serde_json::to_string(self).expect("Could not serialize");
+        ::serde_json::to_string(self).expect("Could not serialize")
+    }
+    
+    
+    /// The retrieve_cookie() method deserializes a string
+    /// into a cookie data type.
+    /// 
+    /// Again, serde is likely to be used here.
+    /// Either the messagepack or json formats would work well here.
+    /// 
+    /// Json is portable and human readable.  
+    /// 
+    /// MsgPack is a binary format, and while not human readable is more
+    /// compact and efficient.
+    #[allow(unused_variables)]
+    // fn retrieve_cookie(string: String) -> Option<Self::CookieType> {
+    fn retrieve_cookie(string: String) -> Option<Self> {
+        let mut des_buf = string.clone();
+        let des: Result<AdministratorCookie, _> = ::serde_json::from_str(&mut des_buf);
+        if let Ok(cooky) = des {
+            Some(cooky)
+        } else {
+            None
+        }
+        // Some(
+        //     AdministratorCookie {
+        //         userid: 66,
+        //         username: "andrew".to_string(),
+        //         display: Some("Andrew Prindle".to_string()),
+        //     }
+        // )
+    }
+}
+
+impl AuthorizeForm for AdministratorForm {
+    type CookieType = AdministratorCookie;
+    
+    /// Authenticate the credentials inside the login form
+    fn authenticate(&self) -> Result<Self::CookieType, AuthFail> {
+        println!("Authenticating {} with password: {}", &self.username, &self.password);
+        if &self.username == "administrator" && &self.password != "" {
+            Ok(
+                AdministratorCookie {
+                    userid: 1,
+                    username: "administrator".to_string(),
+                    display: Some("Administrator".to_string()),
+                }
+            )
+        } else {
+            Err(
+                AuthFail::new(self.username.to_string(), "Incorrect username".to_string())
+            )
+        }
+    }
+    
+    /// Create a new login form instance
+    fn new_form(user: &str, pass: &str, _extras: Option<HashMap<String, String>>) -> Self {
+        AdministratorForm {
+            username: user.to_string(),
+            password: pass.to_string(),
+        }
+    }
+    
+}
+
+impl<'a, 'r> FromRequest<'a, 'r> for AdministratorCookie {
+    type Error = ();
+    
+    /// The from_request inside the file defining the custom data types
+    /// enables the type to be checked directly in a route as a request guard
+    /// 
+    /// This is not needed but highly recommended.  Otherwise you would need to use:
+    /// 
+    /// `#[get("/protected")] fn admin_page(admin: AuthCont<AdministratorCookie>)`
+    /// 
+    /// instead of:
+    /// 
+    /// `#[get("/protected")] fn admin_page(admin: AdministratorCookie)`
+    fn from_request(request: &'a Request<'r>) -> ::rocket::request::Outcome<AdministratorCookie,Self::Error>{
+        let cid = AdministratorCookie::cookie_id();
+        let mut cookies = request.cookies();
+        
+        match cookies.get_private(cid) {
+            Some(cookie) => {
+                if let Some(cookie_deserialized) = AdministratorCookie::retrieve_cookie(cookie.value().to_string()) {
+                    Outcome::Success(
+                        cookie_deserialized
+                    )
+                } else {
+                    Outcome::Forward(())
+                }
+            },
+            None => Outcome::Forward(())
+        }
+    }
+}
+
---
+++ example/database_example/src/db.rs
@@ -0,0 +1,120 @@
+
+use rocket;
+use ::rocket::request::{self, FromRequest, FromForm, FormItems};
+use rocket::{Request, State, Outcome};
+use ::rocket::config::{Config, Environment};
+use rocket::http::Status;
+
+
+// use rocket::request::{self, FromRequest};
+
+use r2d2;
+use r2d2_postgres;
+use r2d2_postgres::{PostgresConnectionManager, TlsMode};
+// use postgres::{Connection, TlsMode};
+use postgres::Connection;
+use postgres;
+use postgres::params::{ConnectParams, Host};
+// use postgres::SslMode;
+// use postgres::TlsMode;
+
+use std::ops::Deref;
+use std;
+use std::env;
+// use dotenv::dotenv;
+
+use super::PGCONN;
+
+// use diesel;
+// use diesel::prelude::*;
+// use diesel::pg::PgConnection;
+
+
+// https://github.com/sfackler/rust-postgres/issues/128
+// let stmt = try!(conn.prepare("INSERT INTO foo (bar) VALUES ('baz') RETURNING id"));
+// let id: i32 = try!(stmt.query(&[])).iter().next().unwrap().get(0);
+
+
+
+
+// https://sfackler.github.io/r2d2-postgres/doc/v0.9.2/r2d2_postgres/struct.PostgresConnectionManager.html
+// https://medium.com/@aergonaut/writing-a-github-webhook-with-rust-part-1-rocket-4426dd06d45d
+// https://github.com/aergonaut/railgun/blob/master/src/railgun/db.rs
+
+// pub type ConnectionPool = r2d2::Pool<r2d2_diesel::ConnectionManager<diesel::pg::PgConnection>>;
+
+
+/// Type alias for the r2d2 connection pool. Use this as a State<T> parameter
+/// in handlers that need a database connection.
+type Pool = r2d2::Pool<PostgresConnectionManager>;
+
+/// Creates the database connection pool
+pub fn init_pg_pool() -> Pool {
+    // let conn_str = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let config = r2d2::Config::default();
+    let manager = PostgresConnectionManager::new("postgres://postgres:andrew@localhost/blog", TlsMode::None).unwrap();
+    
+    r2d2::Pool::new(config, manager).expect("Could not create database pool")
+}
+
+// pub fn init_pg_conn() -> Connection {
+//     // let conn_str = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     Connection::connect("postgres://postgres:andrew@localhost/blog", postgres::TlsMode::None).unwrap()
+// }
+
+
+/// DbConn is a data structure that contains a database connection.
+/// The DbConn also has a request guard that retrieves a connection
+/// from the shared state when the route is called.
+pub struct DbConn(
+    pub r2d2::PooledConnection<PostgresConnectionManager>
+);
+
+/// Attempts to retrieve a single connection from the managed database pool. If
+/// no pool is currently managed, fails with an `InternalServerError` status. If
+/// no connections are available, fails with a `ServiceUnavailable` status.
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<DbConn, ()> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+// For the convenience of using an &DbConn as an &SqliteConnection.
+impl Deref for DbConn {
+    // type Target = SqliteConnection;
+    type Target = Connection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+
+pub fn establish_connection() -> Connection {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    // PgConnection::establish(&database_url).expect("Error connecting to {}", database_url);
+    // Connection::connect("postgres://postgres@localhost:5433", TlsMode::None).unwrap()
+    Connection::connect(database_url, postgres::TlsMode::None).unwrap()
+}
+// pub fn establish_connection_dotenv() -> Connection {
+//     dotenv().ok();
+//     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+//     Connection::connect(database_url, postgres::TlsMode::None).unwrap()
+// }
+
+
+pub fn get_connection() -> Connection {
+    let conn = PGCONN.lock().unwrap();
+    conn
+}
+
+
+
+
